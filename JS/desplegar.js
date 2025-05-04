4b73992b173098fbf0aea1f710cdf6ff2f1b217d
Lucianom26 <lucianomichellon@gmail.com>
2025-05-04 16:43:20 -0300

    cambios

--- JS/desplegar.js
+++ JS/desplegar.js
@@ -10,25 +10,28 @@ document.addEventListener('DOMContentLoaded', () => {
       // Si el contenido ya está abierto, lo cerramos
       if (target.classList.contains('activo')) {
         target.classList.remove('activo');
+        this.classList.remove('activo'); // Remover la clase activa de este enlace
       } else {
         // Cerramos todos los contenidos
         document.querySelectorAll('.contenido').forEach(c => c.classList.remove('activo'));
+        document.querySelectorAll('.enlace').forEach(enlace => enlace.classList.remove('activo')); // Remover la clase activa de todos los enlaces
         
         // Abrimos el contenido seleccionado
         target.classList.add('activo');
+        this.classList.add('activo'); // Agregar la clase activa a este enlace para cambiar la imagen
       }
       
-   // Cerramos otros contenidos si están abiertos (para asegurar que solo uno esté abierto)
-   document.querySelectorAll('.contenido').forEach(c => {
-    if (c !== target) { // Excluimos el contenido actual
-      c.classList.remove('activo');
-    }
-  });
-  
+      // Cerramos otros contenidos si están abiertos (para asegurar que solo uno esté abierto)
+      document.querySelectorAll('.contenido').forEach(c => {
+        if (c !== target) { // Excluimos el contenido actual
+          c.classList.remove('activo');
+        }
+      });
     });
   });
 });
 
+// Manejo de la lógica del botón "Ver más"
 document.addEventListener('DOMContentLoaded', () => {
   const botonMas = document.querySelector('.ir-a-mas');
   const bloquesMas = document.querySelectorAll('.bloque-mas');
