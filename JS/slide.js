666eefa30e6b43405893e2a86aaaa7d270999d6f
Lucianom26 <lucianomichellon@gmail.com>
2025-05-13 18:55:14 -0300

    Cambios-finales

--- JS/slide.js
+++ JS/slide.js
@@ -1,30 +1,20 @@
-const container = document.getElementById("slideshow-container");
-  const totalSlides = 50;
+let slideIndex = 0;
+showSlides();
 
-  // Crear dinámicamente 45 slides
-  for (let i = 1; i <= totalSlides; i++) {
-    const slide = document.createElement("div");
-    slide.className = "slide";
-    if (i === 1) slide.classList.add("active");
-
-    const img = document.createElement("img");
-    img.src = `imagen/slide/page${i}.png`; // Asegúrate de tener page1.jpg hasta page45.jpg
-    img.alt = `Página ${i}`;
-    img.loading = "lazy";
-
-    slide.appendChild(img);
-    container.insertBefore(slide, container.children[0]); // Inserta antes de las flechas
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-
-  let currentSlide = 0;
-  const slides = document.querySelectorAll(".slide");
-
-  function showSlide(index) {
-    slides.forEach(slide => slide.classList.remove("active"));
-    slides[index].classList.add("active");
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
   }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
-  function changeSlide(step) {
-    currentSlide = (currentSlide + step + slides.length) % slides.length;
-    showSlide(currentSlide);
-  }
